142b57fc3cd6535ef1de3a1200ad0a227b2d1ee3
aindriu80 <aindriu80@gmail.com>
2017-10-06 10:06:14 +0100

    Array Randomizer

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,16 +1,38 @@
-var titleImages  =[];
+var tileImages  =[];
+var tileArray = [];
 var startButton = document.getElementById('start')
+var gamePlay = false; // controls if we rebuild the board restart
 
 //event listeners
 startButton.addEventListener('click', startGame);
 
 
-for (var x = 1; x <7; x++){
-    titleImages.push(x+' .jpg');
-}
-
 //Functions
 function startGame(){
     startButton.style.display='none';
+    if(!gamePlay){
+        gamePlay = true;
+        buildArray();
+        tileArray = tileImages.concat(tileImages);
+        shuffleArray(tileArray);
+        console.log(tileArray);
+    }
     console.log('started');
+}
+
+function buildArray() {
+  for (var x = 1; x < 7; x++) {
+    tileImages.push(x + '.jpg');
+  }
+}
+
+function shuffleArray(array){
+    for(var x = array.length -1; x>0; x--){
+        var holder = Math.floor(Math.random() * (x+1) );        
+        var itemValue = array[x];
+        array[x] = array[holder];
+        array[holder] = itemValue;
+        console.log(itemValue);
+    }
+    return array;
 }
